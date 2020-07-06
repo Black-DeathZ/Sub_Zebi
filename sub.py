ba613483663c220686e0f5d19fff33e13b500a02
Black-DeathZ <60795188+Black-DeathZ@users.noreply.github.com>
2020-07-06 22:10:44 +0100

    updated

---
+++ sub.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import requests
+import os
+import time
+print """
+                                    /)
+                                   //
+               __卐__             //
+              /-(____)           //
+             ////- -|\          //
+          ,____o% -,_          //
+         /  \\   |||  ;        //
+        /____\....::./\      //
+       _/__/#\_ _,,_/--\    //
+       /___/######## \/""-(\ )
+     /___\  __  /___\/     |
+    /____\\'__'//____\ _ _ _|
+
+"""
+
+hr = raw_input("Enter site name without www :")
+sub_list = open("subs.txt").read()
+subs = sub_list.splitlines()
+os.system("clear")
+for sub in subs:
+    sb_check = ("http://"+sub+"."+hr)
+    try:
+        requests.get(sb_check)
+    except requests.ConnectionError:
+        pass
+    else:
+
+        print "[+] Valid ==>: ",sb_check
